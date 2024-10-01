4debd43d07dad38713da4e3b972b9f177df01307
Atal Raj Paudel <155559799+AtalRajPaudel@users.noreply.github.com>
2024-09-30 19:54:12 -0500

    Update main.py
    commented on the lines

--- PersonalExpenseTracker/main.py
+++ PersonalExpenseTracker/main.py
@@ -5,5 +5,5 @@ users = {}
 def register_user():
     username = input("Enter username: ")
     password = input("Enter password: ")
-    users[username] = User(username,password)
+    users[username] = User(username,password) ##Here value is stored in the dictionary with the key being the username and value being the object
     print("User registered successfully")
